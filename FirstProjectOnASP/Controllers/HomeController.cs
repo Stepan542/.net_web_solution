9446c7b896227ed19f7384270c150cb01b0cec58
Степан Макаров <Stepan>
2024-09-25 01:56:46 +0500

    Initial commit

---
+++ FirstProjectOnASP/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectOnASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstProjectOnASP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mime;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
